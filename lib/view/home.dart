3b02359ed790d97f44eeb1a4de38718777f1e53a
adinanhamza <adinanck4557@gmail.com>
2025-04-24 21:22:03 +0530

    adding

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -5,58 +5,67 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Colors.white,
-      //   leading: Icon(Icons.notifications_none,color: Colors.black54,),
-      //   title: Column(
-      //     children: [
-      //       Text('your location',style: TextStyle(color: Colors.black45),),
-      //       Text('Canannore,Kerala',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)
-      //     ],
-      //   ),
-      //   actions: [
-      //     ClipRect(
-      //       child: Image.network('https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
-      //     )
-      //   ],
-
-      // ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-            
-                children: [
-                  Icon(Icons.notifications_none,color: Colors.black,),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('your location',style: TextStyle(color: Colors.black45),),
-            Row(
-              children: [
-                Text('Canannore,Kerala',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),),
-                Icon(Icons.keyboard_arrow_down_sharp)
-              ],
-            )
-          ],
-        ),
-        ClipRect(
-            child: Container(decoration:BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              
-            ),child:  Image.network('https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',fit: BoxFit.cover,width: 50,height: 50,),),
-          )
-      
-                ],
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                  
+                    Row(
+                      children: [
+                          Container(child: Center(child: Icon(Icons.notifications_none, color: Colors.black)),
+                       decoration: BoxDecoration(border: Border.all(),borderRadius: BorderRadius.circular(15),),  height:50,width: 50, ),
+                          SizedBox(width: 15,),
+                        Column(
+                          // mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'your location',
+                              style: TextStyle(color: Colors.black45),
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Canannore,Kerala',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Icon(Icons.keyboard_arrow_down_sharp),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 30,),
+                    ClipRect(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15)
+                        ),
+                        child: Image.network(
+                          'https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+                          fit: BoxFit.cover,
+                          width: 50,
+                          height: 50,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
