13af0d266d730fe181b2b82ae63caa8bc9ce4dea
adinanhamza <adinanck4557@gmail.com>
2025-04-24 20:48:12 +0530

    adding ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rental_cars/view/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      home: Home(),
     );
   }
 }
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -5,6 +5,58 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(child: Scaffold(
+      // appBar: AppBar(
+      //   backgroundColor: Colors.white,
+      //   leading: Icon(Icons.notifications_none,color: Colors.black54,),
+      //   title: Column(
+      //     children: [
+      //       Text('your location',style: TextStyle(color: Colors.black45),),
+      //       Text('Canannore,Kerala',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)
+      //     ],
+      //   ),
+      //   actions: [
+      //     ClipRect(
+      //       child: Image.network('https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+      //     )
+      //   ],
+
+      // ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+            
+                children: [
+                  Icon(Icons.notifications_none,color: Colors.black,),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('your location',style: TextStyle(color: Colors.black45),),
+            Row(
+              children: [
+                Text('Canannore,Kerala',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),),
+                Icon(Icons.keyboard_arrow_down_sharp)
+              ],
+            )
+          ],
+        ),
+        ClipRect(
+            child: Container(decoration:BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              
+            ),child:  Image.network('https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',fit: BoxFit.cover,width: 50,height: 50,),),
+          )
+      
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
